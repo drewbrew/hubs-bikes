aaddaa4ada21aacb6fdd73c4eae602fe3ec883e6
Drew Winstel <drew@hsv.beer>
2021-02-22 17:10:12 -0600

    Add new fields to the bike model and rearrange the admin view
    As requested by Larry

--- bikes/admin.py
+++ bikes/admin.py
@@ -1,11 +1,52 @@
 from django.contrib import admin
 
-from .models import Bike, Make, Repair
+from .models import Bike, Donor, Make, Repair
 
 
 class BikeAdmin(admin.ModelAdmin):
     list_select_related = ["make"]
     list_display = ["id", "make", "model", "serial_number", "status"]
+    date_hierarchy = "intake_time"
+    fieldsets = [
+        (
+            "Basic information",
+            {
+                "fields": (
+                    "make",
+                    "model",
+                    "serial_number",
+                    "picture",
+                    "status",
+                    "donated_by",
+                )
+            },
+        ),
+        (
+            "More details",
+            {
+                "fields": (
+                    "condition",
+                    "frame_size",
+                    "wheel_size",
+                    "tube_fitting",
+                    "brake_type",
+                )
+            },
+        ),
+        ("Repair info", {"fields": ("refurb_parts_used", "notes")}),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "intake_time",
+                    "marked_as_needing_repair_time",
+                    "marked_as_ready_for_sale_time",
+                    "sold_time",
+                    "dismantled_time",
+                )
+            },
+        ),
+    ]
 
 
 class RepairAdmin(admin.ModelAdmin):
@@ -13,6 +54,7 @@ class RepairAdmin(admin.ModelAdmin):
     list_display = ["id", "bike", "user", "time_finished"]
 
 
+admin.site.register(Donor)
 admin.site.register(Make)
 admin.site.register(Bike, BikeAdmin)
 admin.site.register(Repair, RepairAdmin)
