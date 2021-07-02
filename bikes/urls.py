af8bfa3888f45c06444e5eb484d3ad5de870e3f4
Drew Winstel <drew@hsv.beer>
2021-07-01 20:45:29 -0500

    Wire in bike create/update URLs

--- bikes/urls.py
+++ bikes/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
 from .users.views import UserViewSet, UserCreateViewSet
-from .views import home
+from .views import home, BikeCreateView, BikeUpdateView
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -18,6 +18,8 @@ urlpatterns = [
     path("api/v1/", include(router.urls)),
     path("api-token-auth/", views.obtain_auth_token),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("bikes/", BikeCreateView.as_view(), name="bikes-create"),
+    path("bikes/<int:pk>", BikeUpdateView.as_view(), name="bikes-update"),
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
     path("", home),
